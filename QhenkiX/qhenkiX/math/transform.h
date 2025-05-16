2bdbc48e102c88b9652c3f59136cad57d67e9ac9
AaronTian-stack <aarontian8@gmail.com>
2025-05-15 21:59:53 -0400

    basis

--- QhenkiX/qhenkiX/math/transform.h
+++ QhenkiX/qhenkiX/math/transform.h
@@ -1,28 +1,68 @@
 ﻿#pragma once
 #include "basis.h"
 
-struct Transform
+namespace qhenki
 {
-	Basis basis_;
-	XMFLOAT3 translation_;
+	struct TransformSIMD
+	{
+		XMMATRIX basis_;
+		XMVECTOR translation_;
+	};
 
-	// Assumes basis is orthonormalized (faster)
-	void invert();
-	Transform invert() const;
+	class Transform
+	{
+		// Assumes basis is orthonormalized (faster)
+		void invert();
+		Transform invert() const;
 
-	// Can handle scale
-	void affine_invert();
-	Transform affine_invert() const;
+		// Can handle scale
+		void affine_invert();
+		Transform affine_invert() const;
 
-	XMMATRIX to_matrix_simd() const;
-	XMFLOAT4X4 to_matrix() const;
+	public:
+		Basis basis_;
+		XMFLOAT3 translation_;
 
-	void translate_local(const XMFLOAT3& t);
-	void translate_global(const XMFLOAT3& t);
+		XMMATRIX to_matrix_simd() const;
 
-	// TODO: operators
+		// inverse transform direction (no scale)
+		/**
+		 * Multiplies inverse of transform with direction, does not consider scale.
+		 * @param d Direction to transform
+		 * @return 
+		 */
+		XMVECTOR inverse_transform_direction(const XMFLOAT3& d) const;
+		/**
+		 * Multiplies inverse of transform with point, considers scale.
+		 * @param p Point to transform
+		 * @return 
+		 */
+		XMVECTOR inverse_transform_point(const XMFLOAT3& p) const;
+		/**
+		 * Multiplies inverse of transform with vector, considers scale.
+		 * @param v Vector to transform
+		 * @return 
+		 */
+		XMVECTOR inverse_transform_vector(const XMFLOAT3& v) const;
 
-	Transform() : basis_(Basis::identity()), translation_(0.f, 0.f, 0.f) {}
-	Transform(const XMFLOAT3& translation) : basis_(Basis::identity()), translation_(translation) {}
-	Transform(const Basis& basis, const XMFLOAT3& translation) : basis_(basis), translation_(translation) {}
-};
+		Transform& look_at(const XMFLOAT3& p, const XMFLOAT3& up);
+
+		// void rotate_around_local(const XMFLOAT3& pivot, const XMFLOAT3& local_axis, float angle);
+		void rotate_around(const XMFLOAT3& pivot, const XMFLOAT3& global_axis, float angle);
+
+		XMVECTOR transform_direction(const XMFLOAT3& d) const;
+		XMVECTOR transform_point(const XMFLOAT3& p) const;
+		XMVECTOR transform_vector(const XMFLOAT3& v) const;
+
+		void translate_local(const XMFLOAT3& t);
+		void translate_global(const XMFLOAT3& t);
+
+		XMVECTOR operator*(const XMFLOAT3& rhs) const;
+		Transform operator*(const Transform& rhs) const;
+		Transform& operator*=(const Transform& rhs);
+
+		Transform() : basis_(Basis::identity()), translation_(0.f, 0.f, 0.f) {}
+		Transform(const XMFLOAT3& translation) : basis_(Basis::identity()), translation_(translation) {}
+		Transform(const Basis& basis, const XMFLOAT3& translation) : basis_(basis), translation_(translation) {}
+	};
+}
