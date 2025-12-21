b399638646a1d52f0553b89f6bd8d05050d26731
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2025-12-21 17:35:49 -0500

    fix for linux runner

--- format-all.ps1
+++ format-all.ps1
@@ -4,15 +4,22 @@ if (-not $clangFormatPath) {
     exit 1
 }
 
+$externalPattern = '[\\/]external[\\/]'
+$buildPattern = '[\\/]build[\\/]'
+$includePattern = '[\\/]include[\\/]'
+$libPattern = '[\\/]lib[\\/]'
+$redistPattern = '[\\/]redist[\\/]'
+$x64Pattern = '[\\/]x64[\\/]'
+
 $files = @(
-    @(Get-ChildItem -Path "QhenkiX" -Include "*.cpp", "*.h" -Recurse | Where-Object { 
-        $_.FullName -notlike "*\external\*" -and $_.FullName -notlike "*\build\*" 
+    @(Get-ChildItem -Path "QhenkiX" -Include "*.cpp", "*.h" -Recurse | Where-Object {
+        $_.FullName -notmatch $externalPattern -and $_.FullName -notmatch $buildPattern
     }),
-    @(Get-ChildItem -Path "Examples" -Include "*.cpp", "*.h" -Recurse | Where-Object { 
-        $_.FullName -notlike "*\include\*" -and $_.FullName -notlike "*\build\*" 
+    @(Get-ChildItem -Path "Examples" -Include "*.cpp", "*.h" -Recurse | Where-Object {
+        $_.FullName -notmatch $includePattern -and $_.FullName -notmatch $buildPattern
     }),
-    @(Get-ChildItem -Path "SXC" -Include "*.cpp", "*.h" -Recurse | Where-Object { 
-        $_.FullName -notlike "*\include\*" -and $_.FullName -notlike "*\build\*" -and $_.FullName -notlike "*\lib\*" -and $_.FullName -notlike "*\redist\*" -and $_.FullName -notlike "*\x64\*"
+    @(Get-ChildItem -Path "SXC" -Include "*.cpp", "*.h" -Recurse | Where-Object {
+        $_.FullName -notmatch $includePattern -and $_.FullName -notmatch $buildPattern -and $_.FullName -notmatch $libPattern -and $_.FullName -notmatch $redistPattern -and $_.FullName -notmatch $x64Pattern
     })
 ) | Where-Object { $_ -ne $null }
 
