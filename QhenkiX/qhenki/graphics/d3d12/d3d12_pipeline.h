bf25b0936e8b8896cd30f78b21061ebffd7f8676
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2025-12-23 17:42:42 -0500

    finish removing defer

--- QhenkiX/qhenki/graphics/d3d12/d3d12_pipeline.h
+++ QhenkiX/qhenki/graphics/d3d12/d3d12_pipeline.h
@@ -9,9 +9,8 @@ namespace qhenki::gfx
 {
 struct D3D12Pipeline
 {
-    std::vector<D3D12_INPUT_ELEMENT_DESC> input_layout_desc;                        // Clear this after creation!
-    D3D12_GRAPHICS_PIPELINE_STATE_DESC* desc = nullptr;                             // Temp for deferred compilation
+    std::vector<D3D12_INPUT_ELEMENT_DESC> input_layout_desc; // TODO: Delete and replace with Arena
+    ComPtr<ID3D12PipelineState> pipeline_state{};
     D3D12_PRIMITIVE_TOPOLOGY primitive_topology = D3D_PRIMITIVE_TOPOLOGY_UNDEFINED; // Needed for command list
-    ComPtr<ID3D12PipelineState> pipeline_state = nullptr;
 };
 } // namespace qhenki::gfx
