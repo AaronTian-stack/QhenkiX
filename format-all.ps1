6318cc07a4b9f916f3cb1c6a1056d8a26f95a104
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2026-02-21 16:51:17 -0500

    exclude external dir from format

--- format-all.ps1
+++ format-all.ps1
@@ -22,10 +22,10 @@ $files = @(
         $_.FullName -notmatch $externalPattern -and $_.FullName -notmatch $buildPattern
     }),
     @(Get-ChildItem -Path "Examples" -Include "*.cpp", "*.h", "*.hlsl" -Recurse | Where-Object {
-        $_.FullName -notmatch $includePattern -and $_.FullName -notmatch $buildPattern
+        $_.FullName -notmatch $includePattern -and $_.FullName -notmatch $buildPattern -and $_.FullName -notmatch $externalPattern
     }),
     @(Get-ChildItem -Path "SXC" -Include "*.cpp", "*.h", "*.hlsl" -Recurse | Where-Object {
-        $_.FullName -notmatch $includePattern -and $_.FullName -notmatch $buildPattern -and $_.FullName -notmatch $libPattern -and $_.FullName -notmatch $redistPattern -and $_.FullName -notmatch $x64Pattern
+        $_.FullName -notmatch $includePattern -and $_.FullName -notmatch $buildPattern -and $_.FullName -notmatch $externalPattern -and $_.FullName -notmatch $libPattern -and $_.FullName -notmatch $redistPattern -and $_.FullName -notmatch $x64Pattern
     })
 ) | Where-Object { $_ -ne $null }
 
