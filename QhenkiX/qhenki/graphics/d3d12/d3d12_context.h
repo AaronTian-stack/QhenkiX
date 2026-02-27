7b4a1e3467710a7f4cf2183179082f4aeefbdbbb
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2026-02-27 01:01:52 -0500

    Descriptor rework (#68)
    * use bytes
    
    * add get descriptor size function
    
    * set type
    
    * bloat descriptors
    
    * update descriptor heap
    
    * add alignment
    
    * partially update examples
    
    need to change copy_desciptors
    
    * change copy
    
    * update examples

--- QhenkiX/qhenki/graphics/d3d12/d3d12_context.h
+++ QhenkiX/qhenki/graphics/d3d12/d3d12_context.h
@@ -94,14 +94,17 @@ public:
     bool create_descriptor_heap(const DescriptorHeapDesc& desc,
                                 DescriptorHeap* heap,
                                 const char* debug_name = nullptr) override;
+    size_t get_descriptor_heap_max_size(DescriptorHeapDesc::Type type) override;
     void set_descriptor_heap(CommandList* cmd_list, const DescriptorHeap& heap) override;
     void set_descriptor_heap(CommandList* cmd_list,
                              const DescriptorHeap& heap,
                              const DescriptorHeap& sampler_heap) override;
 
     void set_descriptor_table(CommandList* cmd_list, unsigned index, const Descriptor& gpu_descriptor) override;
-    bool copy_descriptors(unsigned count, const Descriptor& src, const Descriptor& dst) override;
+    bool copy_descriptors(size_t bytes, const Descriptor& src, const Descriptor& dst) override;
     bool free_descriptor(Descriptor* descriptor) override;
+    size_t get_descriptor_size(Descriptor::Type type) const override;
+    size_t get_descriptor_alignment(Descriptor::Type type) const override;
 
     bool create_buffer(const BufferDesc& desc, const void* data, Buffer* buffer, const char* debug_name) override;
     bool create_descriptor_constant_view(const Buffer& buffer, DescriptorHeap* heap, Descriptor* descriptor) override;
