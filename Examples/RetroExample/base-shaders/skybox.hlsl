ecaf809ede56d52cb9608f345d54ee66ec39ff95
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2026-04-11 18:30:22 -0400

    use correct sampler

--- Examples/RetroExample/base-shaders/skybox.hlsl
+++ Examples/RetroExample/base-shaders/skybox.hlsl
@@ -57,7 +57,7 @@ float4 ps_main(PSInput input)
     float dot_radius = 0.3;
     float dotted = 1.0 - step(dot_radius, dist);
 
-    float3 tex = g_texture.Sample(samp_linear, p).rgb;
+    float3 tex = g_texture.Sample(samp, p).rgb;
 
     float dist_from_mid = 2.0 * abs(uv.y - 0.5);
     float alpha_fade = 1.0 - smoothstep(0.5, 1.0, dist_from_mid);
