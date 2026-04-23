ddb58b633fab5d867ba7aa4c38db014ed50c9aaa
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2026-04-22 22:40:34 -0400

    separate out sm name util

--- QhenkiX/qhenki/utility/d3d_util.cpp
+++ QhenkiX/qhenki/utility/d3d_util.cpp
@@ -1,7 +1,6 @@
 #include "d3d_util.h"
 
 #include <magic_enum/magic_enum.hpp>
-#include <stdexcept>
 #include "qhenki/RHI/shader.h"
 
 namespace qhenki::gfx
@@ -129,80 +128,6 @@ X(INVERT_CONSTANT_COLOR, D3D12_BLEND_INV_BLEND_FACTOR) \
     X(AND_INVERTED, D3D12_LOGIC_OP_AND_INVERTED)   \
     X(OR_REVERSE, D3D12_LOGIC_OP_OR_REVERSE)       \
     X(OR_INVERTED, D3D12_LOGIC_OP_OR_INVERTED)
-
-std::wstring shader_model_wchar(const ShaderType type, const ShaderModel model)
-{
-    auto sm = magic_enum::enum_name(model);
-    assert(sm.size() == 6);
-
-    if (type == LIBRARY_SHADER)
-    {
-        // "SM_6_6" -> L"lib_6_6"
-        const auto underscore = sm.find('_');
-        assert(underscore != std::string_view::npos);
-        std::wstring smc = L"lib";
-        smc.push_back(L'_');
-        smc.append(std::wstring(sm.begin() + underscore + 1, sm.end()));
-        return smc;
-    }
-
-    std::wstring smc(sm.begin(), sm.end());
-    smc[1] = 's';
-
-    switch (type)
-    {
-    case VERTEX_SHADER:
-        smc[0] = 'v';
-        break;
-    case PIXEL_SHADER:
-        smc[0] = 'p';
-        break;
-    case COMPUTE_SHADER:
-        smc[0] = 'c';
-        break;
-    case LIBRARY_SHADER:
-        break;
-    }
-
-    return smc;
-}
-
-std::string shader_model_char(const ShaderType type, const ShaderModel model)
-{
-    auto sm = magic_enum::enum_name(model);
-    assert(sm.size() == 6);
-
-    if (type == LIBRARY_SHADER)
-    {
-        // "SM_6_6" -> "lib_6_6"
-        const auto underscore = sm.find('_');
-        assert(underscore != std::string_view::npos);
-        std::string smc = "lib";
-        smc.push_back('_');
-        smc.append(std::string(sm.begin() + underscore + 1, sm.end()));
-        return smc;
-    }
-
-    auto smc = std::string(sm); // Should not cause heap allocation (6 chars)
-    smc[1] = 's';
-
-    switch (type)
-    {
-    case VERTEX_SHADER:
-        smc[0] = 'v';
-        break;
-    case PIXEL_SHADER:
-        smc[0] = 'p';
-        break;
-    case COMPUTE_SHADER:
-        smc[0] = 'c';
-        break;
-    case LIBRARY_SHADER:
-        break;
-    }
-
-    return smc;
-}
 
 DXGI_FORMAT dxgi_format(const IndexType format)
 {
