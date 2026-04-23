bf8aa71a1bf6f85527d31c1bc64b93d1e5708642
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2026-04-22 23:58:30 -0400

    move throw macro

--- QhenkiX/include/qhenki/utility/general_util.h
+++ QhenkiX/include/qhenki/utility/general_util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <stdexcept>
 
 namespace qhenki::util
 {
@@ -14,3 +15,21 @@ template<typename T, typename... Ts> auto ptr_array(T& v, Ts&... vs)
     return std::array<T*, sizeof...(Ts) + 1>{&v, &vs...};
 }
 } // namespace qhenki::util
+
+#define THROW_IF_FALSE(result)                                   \
+    do                                                           \
+    {                                                            \
+        if (!result)                                             \
+        {                                                        \
+            throw std::runtime_error("Something went wrong!\n"); \
+        }                                                        \
+    } while (0)
+
+#define THROW_IF_TRUE(result)                                    \
+    do                                                           \
+    {                                                            \
+        if ((result))                                            \
+        {                                                        \
+            throw std::runtime_error("Something went wrong!\n"); \
+        }                                                        \
+    } while (0)
